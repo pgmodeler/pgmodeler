453251462dd6db398cfeaa74ad4a2ba55d702d81
Raphael A. Silva <rkhaotix@gmail.com>
2012-10-07 11:48:21 -0300

    Fixed build and startup on Windows system.

--- start-pgmodeler.bat
+++ start-pgmodeler.bat
@@ -1,7 +1,10 @@
-set PGMODELER_ROOT="C:\Users\raphael\Desktop\pgmodeler-code\build"
-set PGMODELER_CONFIGS_DIR=%PGMODELER_ROOT%\conf
+set PGMODELER_ROOT=C:\Users\raphael\Desktop\pgmodeler\build
+set PGMODELER_CONF_DIR=%PGMODELER_ROOT%\conf
 set PGMODELER_SCHEMAS_DIR=%PGMODELER_ROOT%\schemas
 set PGMODELER_LANG_DIR=%PGMODELER_ROOT%\lang
 set PGMODELER_PLUGINS_DIR=%PGMODELER_ROOT%\plugins
+set PGMODELER_LIB_DIR=%PGMODELER_ROOT%\lib
+
+set PATH=C:\QtSDK\mingw\bin;%PGMODELER_LIB_DIR%;C:\QtSDK\Desktop\Qt\4.8.1\mingw\lib;C:\PostgreSQL\9.2\lib;C:\PostgreSQL\9.2\bin;%PATH%
 
 %PGMODELER_ROOT%\pgmodeler.exe
