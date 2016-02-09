c44f0fed3d4f3fc5cff31a317a8ef16d6ba00ac9
Raphael A. Silva <rkhaotix@gmail.com>
2016-02-09 18:12:14 -0300

    Fixed the object search in DatabaseModel. Now duplicated operator families are accepted, the desambiguation term will be the indexing mode.
    Fixed the creation of operator classes. Now operator families are
    referenced by their signature not the name.

--- libpgmodeler/src/databasemodel.cpp
+++ libpgmodeler/src/databasemodel.cpp
@@ -583,20 +583,16 @@ BaseObject *DatabaseModel::getObject(const QString &name, ObjectType obj_type, i
 		throw Exception(ERR_OBT_OBJ_INVALID_TYPE,__PRETTY_FUNCTION__,__FILE__,__LINE__);
 	else
 	{
-    /* This regexp is used for operator class and operator family.
-       Their signature comes with a "USING index_mode" string
-       that must be removed before any comparison is done */
-    QRegExp aux_regexp(QString("( )+(USING)(.)+"));
     QString signature;
 
     itr=obj_list->begin();
 		itr_end=obj_list->end();
 		obj_idx=-1;		
-    aux_name1=QString(name).remove('"').remove(aux_regexp);
+    aux_name1=QString(name).remove('"');
 
     while(itr!=itr_end && !found)
     {
-      signature=(*itr)->getSignature().remove("\"").remove(aux_regexp);
+      signature=(*itr)->getSignature().remove("\"");
       found=(signature==aux_name1);
       if(!found) itr++;
     }
@@ -4320,14 +4316,14 @@ OperatorClass *DatabaseModel::createOperatorClass(void)
 					if(elem==objs_schemas[OBJ_OPFAMILY])
 					{
 						xmlparser.getElementAttributes(attribs);
-						object=getObject(attribs[ParsersAttributes::NAME], OBJ_OPFAMILY);
+            object=getObject(attribs[ParsersAttributes::SIGNATURE], OBJ_OPFAMILY);
 
 						//Raises an error if the operator family doesn't exists
-						if(!object && !attribs[ParsersAttributes::NAME].isEmpty())
+            if(!object && !attribs[ParsersAttributes::SIGNATURE].isEmpty())
 							throw Exception(Exception::getErrorMessage(ERR_REF_OBJ_INEXISTS_MODEL)
                               .arg(op_class->getName())
 															.arg(op_class->getTypeName())
-                              .arg(attribs[ParsersAttributes::NAME])
+                              .arg(attribs[ParsersAttributes::SIGNATURE])
 								.arg(BaseObject::getTypeName(OBJ_OPFAMILY)),
 								ERR_REF_OBJ_INEXISTS_MODEL,__PRETTY_FUNCTION__,__FILE__,__LINE__);
 
@@ -4371,13 +4367,13 @@ OperatorClass *DatabaseModel::createOperatorClass(void)
 								xmlparser.accessElement(XMLParser::NEXT_ELEMENT);
 								xmlparser.getElementAttributes(attribs_aux);
 
-								object=getObject(attribs_aux[ParsersAttributes::NAME],OBJ_OPFAMILY);
+                object=getObject(attribs_aux[ParsersAttributes::SIGNATURE],OBJ_OPFAMILY);
 
-								if(!object && !attribs_aux[ParsersAttributes::NAME].isEmpty())
+                if(!object && !attribs_aux[ParsersAttributes::SIGNATURE].isEmpty())
 									throw Exception(Exception::getErrorMessage(ERR_REF_OBJ_INEXISTS_MODEL)
                                   .arg(op_class->getName())
 																	.arg(op_class->getTypeName())
-                                  .arg(attribs_aux[ParsersAttributes::NAME])
+                                  .arg(attribs_aux[ParsersAttributes::SIGNATURE])
 																	.arg(BaseObject::getTypeName(OBJ_OPFAMILY)),
 										ERR_REF_OBJ_INEXISTS_MODEL,__PRETTY_FUNCTION__,__FILE__,__LINE__);
 
