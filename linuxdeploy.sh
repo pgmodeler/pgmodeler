18fceb54d4959e821d07fe6285be30ff8718aa25
Raphael A. Silva <rkhaotix@gmail.com>
2016-03-02 11:09:47 -0300

    Changed the default Qt version to 5.5.1 in linuxdeploy.sh

--- linuxdeploy.sh
+++ linuxdeploy.sh
@@ -4,13 +4,13 @@
 case `uname -m` in
   "x86_64")
     ARCH="linux64"
-    FALLBACK_QT_ROOT=/opt/qt-5.4.1/5.4/gcc_64
+    FALLBACK_QT_ROOT=/opt/qt-5.5.1/5.5/gcc_64
     FALLBACK_QMAKE_ROOT="$FALLBACK_QT_ROOT/bin"
     ;;
     
    *)
     ARCH="linux32"
-    FALLBACK_QT_ROOT=/opt/qt-5.4.1/5.4/gcc
+    FALLBACK_QT_ROOT=/opt/qt-5.5.1/5.5/gcc
     FALLBACK_QMAKE_ROOT="$FALLBACK_QT_ROOT/bin"
     ;;
 esac
