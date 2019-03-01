b0e2b6c6ffc8c62da37ab1d902fcc61b5063ccef
Raphael A. Silva <rkhaotix@gmail.com>
2019-03-01 14:48:17 -0300

    Finished the result set filtering in SQLExecutionWidget

--- libpgmodeler_ui/src/sqlexecutionwidget.cpp
+++ libpgmodeler_ui/src/sqlexecutionwidget.cpp
@@ -98,8 +98,8 @@ SQLExecutionWidget::SQLExecutionWidget(QWidget * parent) : QWidget(parent)
 
 	connect(columns_cmb, SIGNAL(currentIndexChanged(int)), this, SLOT(filterResults()));
 	connect(filter_edt, SIGNAL(textChanged(QString)), this, SLOT(filterResults()));
-	connect(filter_tb, SIGNAL(toggled(bool)), filter_wgt, SLOT(setVisible(bool)));
 	connect(hide_tb, SIGNAL(clicked(bool)), filter_tb, SLOT(click()));
+	connect(filter_tb, SIGNAL(toggled(bool)), filter_wgt, SLOT(setVisible(bool)));
 
 	connect(exact_chk, SIGNAL(toggled(bool)), this, SLOT(filterResults()));
 	connect(exact_chk, &QCheckBox::toggled, [&](bool checked){
@@ -464,20 +464,17 @@ void SQLExecutionWidget::filterResults(void)
 
 	if(exact_chk->isChecked())
 		flags = Qt::MatchExactly;
-	else
-	{
-		if(regexp_chk->isChecked())
-			flags |= Qt::MatchRegExp;
+	else if(regexp_chk->isChecked())
+			flags = Qt::MatchRegExp;
 
-		if(case_sensitive_chk->isChecked())
-			flags |= Qt::MatchCaseSensitive;
-	}
+	if(case_sensitive_chk->isChecked())
+		flags |= Qt::MatchCaseSensitive;
 
 	list = results_tbw->model()->match(results_tbw->model()->index(0, columns_cmb->currentIndex()),
 																		 Qt::DisplayRole, filter_edt->text(), -1, flags);
 
-	results_tbw->model()->blockSignals(true);
 	results_tbw->blockSignals(true);
+	results_tbw->setUpdatesEnabled(false);
 
 	for(int row = 0; row < rows_cnt; row++)
 		results_tbw->hideRow(row);
@@ -488,8 +485,8 @@ void SQLExecutionWidget::filterResults(void)
 			results_tbw->showRow(idx.row());
 	}
 
-	results_tbw->model()->blockSignals(false);
 	results_tbw->blockSignals(false);
+	results_tbw->setUpdatesEnabled(true);
 	results_tbw->update();
 }
 
@@ -565,6 +562,7 @@ void SQLExecutionWidget::switchToExecutionMode(bool value)
 	sql_cmd_txt->setEnabled(!value);
 	cmd_history_parent->setEnabled(!value);
 	find_history_parent->setEnabled(!value);
+	filter_tb->setEnabled(!value);
 
 	if(value)
 	{
