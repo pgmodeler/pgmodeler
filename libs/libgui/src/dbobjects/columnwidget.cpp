c3aebd2cff78eefda1ed9dbe0d7e72180b702c19
Raphael Araújo e Silva <raphael@pgmodeler.io>
2022-10-27 10:52:24 -0300

    Refactored ColumnWidget signal/slot connection to new syntax

--- libs/libgui/src/dbobjects/columnwidget.cpp
+++ libs/libgui/src/dbobjects/columnwidget.cpp
@@ -57,16 +57,17 @@ ColumnWidget::ColumnWidget(QWidget *parent): BaseObjectWidget(parent, ObjectType
 		fields_map[generateVersionsInterval(AfterVersion, PgSqlVersions::PgSqlVersion120)].push_back(generated_chk);
 		highlightVersionSpecificFields(fields_map);
 
-		connect(expression_rb, SIGNAL(toggled(bool)), this, SLOT(enableDefaultValueFields()));
-		connect(sequence_rb, SIGNAL(toggled(bool)), this, SLOT(enableDefaultValueFields()));
-		connect(identity_rb, SIGNAL(toggled(bool)), this, SLOT(enableDefaultValueFields()));
+		connect(expression_rb, &QRadioButton::toggled, this, &ColumnWidget::enableDefaultValueFields);
+		connect(sequence_rb, &QRadioButton::toggled, this, &ColumnWidget::enableDefaultValueFields);
+		connect(identity_rb, &QRadioButton::toggled, this, &ColumnWidget::enableDefaultValueFields);
 
 		connect(generated_chk, &QCheckBox::toggled, [&](bool value){
 			notnull_chk->setDisabled(value);
 			notnull_chk->setChecked(false);
 		});
 
-		connect(edit_seq_btn, SIGNAL(clicked(bool)), this, SLOT(editSequenceAttributes()));
+		connect(edit_seq_btn, &QPushButton::clicked, this, &ColumnWidget::editSequenceAttributes);
+
 		setMinimumSize(540, 480);
 	}
 	catch(Exception &e)
