d8e2e67239171723b05b6dc74416f8a6059a0b01
Raphael Araújo e Silva <raphael@pgmodeler.io>
2023-02-19 10:12:38 -0300

    Version upgrade 1.0.1 -> 1.0.2

--- libs/libutils/src/globalattributes.cpp
+++ libs/libutils/src/globalattributes.cpp
@@ -21,7 +21,7 @@
 #include <QRegularExpression>
 
 /** Base version number **/
-const QString GlobalAttributes::PgModelerVersion = QString("1.0.1")
+const QString GlobalAttributes::PgModelerVersion = QString("1.0.2")
 
 /* Appending the snapshot build number to the version number
  * when the external variable SNAPSHOT_BUILD is defined */
