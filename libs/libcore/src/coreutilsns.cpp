d7cc9373af66357eee36ef9321772ab2745fec4d
Raphael Araújo e Silva <raphael@pgmodeler.io>
2023-04-13 10:15:32 -0300

    Minor adjust in file coreutilsns.cpp

--- libs/libcore/src/coreutilsns.cpp
+++ libs/libcore/src/coreutilsns.cpp
@@ -16,6 +16,9 @@
 # Also, you can get the complete GNU General Public License at <http://www.gnu.org/licenses/>
 */
 
+#ifndef CORE_UTILS_NS_H
+#define CORE_UTILS_NS_H
+
 #include "coreutilsns.h"
 #include "databasemodel.h"
 #include <QDebug>
@@ -248,3 +251,5 @@ namespace CoreUtilsNs {
 		}
 	}
 }
+
+#endif
