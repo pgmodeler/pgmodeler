cf8039d42a2b26ce6f943e1e91995b74540268b6
Raphael Araújo e Silva <raphael@pgmodeler.io>
2023-10-16 15:41:25 -0300

    Minor comments adjusment

--- libs/libutils/src/pgsqlversions.h
+++ libs/libutils/src/pgsqlversions.h
@@ -46,7 +46,7 @@ namespace PgSqlVersions {
 	/*! \brief Parses and checks a string contaning a PostgreSQL version in the form [major.minor]
 	 * When the flag ignore_legacy_ver is true, this method will raise an error if an invalid version
 	 * is provided. An invalid version is considered any string in incorrect format or a valid string
-	 * but that is for a obsolete version (< 9.0). If an invalid major version is proveded then the function
+	 * but refers to an obsolete version (< 10.0). If an invalid major version is proveded then the function
 	 * will return the DefaultVersion as a fallback. */
 	extern __libutils QString parseString(const QString &pgsql_ver, bool ignore_legacy_ver);
 }
