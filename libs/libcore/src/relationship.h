01df2fcea20f4f695d73bf8a630c52d225bf081f
Raphael Araújo e Silva <raphael@pgmodeler.io>
2024-08-29 08:04:32 -0300

    Reverted inline static declarations in Relationship

--- libs/libcore/src/relationship.h
+++ libs/libcore/src/relationship.h
@@ -328,11 +328,11 @@ class __libcore Relationship: public BaseRelationship {
 
 	public:
 		//! \brief String used as the name suffix separator. Default '_'
-		inline static const QString	SuffixSeparator {"_"},
-		SrcTabToken {"{st}"},
-		DstTabToken {"{dt}"},
-		GenTabToken {"{gt}"},
-		SrcColToken {"{sc}"};
+		static const QString	SuffixSeparator,
+		SrcTabToken,
+		DstTabToken,
+		GenTabToken,
+		SrcColToken;
 
 		Relationship(Relationship *rel);
 
