a84009ffe97444a4de1aba2336e6bf612a0fff84
Raphael Araújo e Silva <raphael@pgmodeler.io>
2024-12-17 10:22:44 -0300

    Fixed a bug in DatabaseModel::updateTableFKRelationships that was causing more FK relationships to be deleted than the what really needed to be.

--- libs/libgui/src/dbobjects/tablewidget.cpp
+++ libs/libgui/src/dbobjects/tablewidget.cpp
@@ -1090,10 +1090,7 @@ void TableWidget::applyConfiguration()
 		}
 		catch(Exception &e)
 		{
-			//Messagebox msg_box;
-
 			if(e.getErrorCode()==ErrorCode::RemInvalidatedObjects)
-				//msg_box.show(e);
 				Messagebox::error(e, __PRETTY_FUNCTION__, __FILE__, __LINE__);
 			else
 				throw Exception(e.getErrorMessage(),e.getErrorCode(),__PRETTY_FUNCTION__,__FILE__,__LINE__, &e);
