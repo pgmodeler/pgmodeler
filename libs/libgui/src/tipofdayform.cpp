13c6625c0415815621fe124c0775aeb30219e834
Raphael Araújo e Silva <raphael@pgmodeler.io>
2025-04-04 16:55:22 -0300

    Starting to add ability to load tip of day contents (wip)

--- libs/libgui/src/tipofdayform.cpp
+++ libs/libgui/src/tipofdayform.cpp
@@ -1,13 +1,60 @@
+/*
+# PostgreSQL Database Modeler (pgModeler)
+#
+# Copyright 2006-2025 - Raphael Araújo e Silva <raphael@pgmodeler.io>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# The complete text of GPLv3 is at LICENSE file on source code root directory.
+# Also, you can get the complete GNU General Public License at <http://www.gnu.org/licenses/>
+*/
+
 #include "tipofdayform.h"
 #include "guiutilsns.h"
+#include <QNetworkReply>
+#include <QNetworkRequest>
 
 TipOfDayForm::TipOfDayForm(QWidget *parent) : QWidget(parent)
 {
 	setupUi(this);
 	setWindowIcon(QIcon(GuiUtilsNs::getIconPath("pgmodeler_logo")));
+
+	net_manager = new QNetworkAccessManager(this);
+
+	connect(net_manager, &QNetworkAccessManager::finished, this, [this](QNetworkReply *reply){
+		if(reply->error() == QNetworkReply::NoError)
+		{
+			textBrowser->setMarkdown(reply->readAll());
+		}
+	});
 }
 
 TipOfDayForm::~TipOfDayForm()
 {
 
+}
+
+void TipOfDayForm::showTipOfDay(const QString &tip)
+{
+
+}
+
+QString TipOfDayForm::loadTipOfDay(const QString &url)
+{
+
+}
+
+void TipOfDayForm::showEvent(QShowEvent *event)
+{
+	if(!event->spontaneous())
+	{
+		net_manager->get(QNetworkRequest(QUrl("http://localhost:8000/checkupdate?version=1.1.0")));
+	}
 }
--- libs/libgui/src/tipofdayform.h
+++ libs/libgui/src/tipofdayform.h
@@ -1,15 +1,45 @@
-#ifndef TIPOFDAYWIDGET_H
-#define TIPOFDAYWIDGET_H
+/*
+# PostgreSQL Database Modeler (pgModeler)
+#
+# Copyright 2006-2025 - Raphael Araújo e Silva <raphael@pgmodeler.io>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# The complete text of GPLv3 is at LICENSE file on source code root directory.
+# Also, you can get the complete GNU General Public License at <http://www.gnu.org/licenses/>
+*/
+
+#ifndef TIPOFDAY_WIDGET_H
+#define TIPOFDAY_WIDGET_H
 
 #include <QWidget>
 #include "ui_tipofdayform.h"
+#include <QNetworkAccessManager>
 
 class TipOfDayForm : public QWidget, public Ui::TipOfDayForm {
 	Q_OBJECT
 
+	private:
+		QNetworkAccessManager *net_manager;
+
+		QString loadTipOfDay(const QString &url);
+
+	protected:
+		void showEvent(QShowEvent *event) override;
+
 	public:
 		explicit TipOfDayForm(QWidget *parent = nullptr);
 		~TipOfDayForm();
+
+	private slots:
+		void showTipOfDay(const QString &tip);
 };
 
-#endif // TIPOFDAYWIDGET_H
+#endif
