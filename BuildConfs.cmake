43c6389a3b9ea6119190a85f2a7049de9ba55fe2
Raphael Araújo e Silva <raphael@pgmodeler.io>
2025-05-13 17:37:48 -0300

    Adjusted the CMakeLists.txt of all tests

--- BuildConfs.cmake
+++ BuildConfs.cmake
@@ -9,7 +9,7 @@ set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
 # Importing Qt6 components
-find_package(Qt6 REQUIRED COMPONENTS Core Gui Network PrintSupport Svg Test Widgets)
+find_package(Qt6 REQUIRED COMPONENTS Core Gui Network PrintSupport Svg Widgets)
 
 qt_standard_project_setup()
 set(CMAKE_AUTORCC ON)
@@ -23,3 +23,58 @@ link_libraries(
     Qt::PrintSupport
     Qt::Svg
     Qt::Widgets)
+
+
+# if(CONFIG___contains___debug)
+#     target_compile_definitions(canvas PUBLIC
+#         PGMODELER_DEBUG
+#     )
+# endif()
+
+#if(NOT defined(NO_CHECK_CURR_VER,var))
+#    target_compile_definitions(canvas PUBLIC
+#        CHECK_CURR_VER
+#    )
+#endif()
+
+# if(SNAPSHOT_BUILD___equals___ON)
+#     target_compile_definitions(canvas PUBLIC
+#         SNAPSHOT_BUILD
+#     )
+# endif()
+
+# if(DEMO_VERSION___equals___ON)
+#     target_compile_definitions(canvas PUBLIC
+#         DEMO_VERSION
+#     )
+# endif()
+
+# if(NO_UPDATE_CHECK___equals___ON)
+#     target_compile_definitions(canvas PUBLIC
+#         NO_UPDATE_CHECK
+#     )
+# endif()
+
+# if(PRIVATE_PLUGINS___equals___ON)
+#     target_compile_definitions(canvas PUBLIC
+#         PRIVATE_PLUGINS_SYMBOLS
+#     )
+# endif()
+
+# if(APPIMAGE_BUILD___equals___ON AND LINUX)
+#     target_compile_definitions(canvas PUBLIC
+#         APPIMAGE_BUILD
+#     )
+# endif()
+
+#if(CMAKE_BUILD_TYPE STREQUAL Debug AND LINUX)
+#    target_compile_options(canvas
+#        -Wall -Wextra -Wuninitialized
+#    )
+#endif()
+
+# if(MACOS)
+#     set_target_properties(canvas PROPERTIES
+#         MACOSX_BUNDLE FALSE
+#     )
+# endif()
