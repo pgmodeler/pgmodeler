41a8b3b4b9178dce81f006d76c9b755480c564d8
Raphael Araújo e Silva <raphael@pgmodeler.io>
2025-05-14 16:43:39 -0300

    Adjusted the use of custom/extra build options in BuildConfs.cmake
    Forcing the Qt version to >= 6.4.3 in find_packages()

--- BuildConfs.cmake
+++ BuildConfs.cmake
@@ -9,10 +9,12 @@ set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
 # Importing Qt6 components
-find_package(Qt6 REQUIRED COMPONENTS Core Gui Network PrintSupport Svg Widgets)
+# Forcing a version >= 6.4.0. If no suitable version is found the script aborts
+find_package(Qt6 6.4.0 REQUIRED COMPONENTS Core Gui Network PrintSupport Svg Widgets)
 
 qt_standard_project_setup()
 
+# Disabling deprecated Qt code of version below 6.4
 add_compile_definitions(QT_DISABLE_DEPRECATED_BEFORE=0x060400)
 
 link_libraries(
@@ -23,54 +25,33 @@ link_libraries(
     Qt::Svg
     Qt::Widgets)
 
+if(CMAKE_BUILD_TYPE STREQUAL Debug)
+    add_compile_definitions(PGMODELER_DEBUG)
+endif()
 
-# if(CONFIG___contains___debug)
-#     target_compile_definitions(canvas PUBLIC
-#         PGMODELER_DEBUG
-#     )
-# endif()
+if(NOT DEFINED NO_CHECK_CURR_VER)
+    add_compile_definitions(CHECK_CURR_VER)
+endif()
 
-#if(NOT defined(NO_CHECK_CURR_VER,var))
-#    target_compile_definitions(canvas PUBLIC
-#        CHECK_CURR_VER
-#    )
-#endif()
+if(SNAPSHOT_BUILD)
+    add_compile_definitions(SNAPSHOT_BUILD)
+endif()
 
-# if(SNAPSHOT_BUILD___equals___ON)
-#     target_compile_definitions(canvas PUBLIC
-#         SNAPSHOT_BUILD
-#     )
-# endif()
+if(DEMO_VERSION)
+    add_compile_definitions(DEMO_VERSION)
+endif()
 
-# if(DEMO_VERSION___equals___ON)
-#     target_compile_definitions(canvas PUBLIC
-#         DEMO_VERSION
-#     )
-# endif()
+if(NO_UPDATE_CHECK)
+    add_compile_definitions(NO_UPDATE_CHECK)
+endif()
 
-# if(NO_UPDATE_CHECK___equals___ON)
-#     target_compile_definitions(canvas PUBLIC
-#         NO_UPDATE_CHECK
-#     )
-# endif()
+if(PRIVATE_PLUGINS)
+    add_compile_definitions(PRIVATE_PLUGINS)
+endif()
 
-# if(PRIVATE_PLUGINS___equals___ON)
-#     target_compile_definitions(canvas PUBLIC
-#         PRIVATE_PLUGINS_SYMBOLS
-#     )
-# endif()
-
-# if(APPIMAGE_BUILD___equals___ON AND LINUX)
-#     target_compile_definitions(canvas PUBLIC
-#         APPIMAGE_BUILD
-#     )
-# endif()
-
-#if(CMAKE_BUILD_TYPE STREQUAL Debug AND LINUX)
-#    target_compile_options(canvas
-#        -Wall -Wextra -Wuninitialized
-#    )
-#endif()
+if(APPIMAGE_BUILD AND LINUX)
+    add_compile_definitions(APPIMAGE_BUILD)
+endif()
 
 # if(MACOS)
 #     set_target_properties(canvas PROPERTIES
