9d6f225878d640c3aa765f46abe9ceee32f6f2eb
Raphael Araújo e Silva <raphael@pgmodeler.io>
2025-05-19 10:52:31 -0300

    Minor fix in libcli/CMakeLists.txt
    Adjusted the development version to 2.0.0-alpha

--- libs/libutils/src/globalattributes.cpp
+++ libs/libutils/src/globalattributes.cpp
@@ -23,7 +23,7 @@
 
 const QString GlobalAttributes::PgModelerVersion {
 	/** Base version number **/
-	QString("2.0.0-alpha1")
+	QString("2.0.0-alpha")
 
 /* Appending the snapshot build number to the version number
  * when the external variable SNAPSHOT_BUILD is defined */
